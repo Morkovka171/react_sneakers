4e1857f1e7e17e27ac0eece42357992205beddab
Morkovka171 <guganowa@yandex.ru>
2023-01-09 12:05:25 +0300

    Фикс картинки

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,7 +9,7 @@ function Header(props) {
     <header className="d-flex justify-between align-center p-40 header">
       <Link to="/">
         <div className="d-flex align-center">
-          <img width={40} height={40} src="/img/logo.png" alt="Logo" />
+          <img width={40} height={40} src='/img/logo.png' alt="Logo" />
           <div>
             <h3 className="text-uppercase">React Sneakers</h3>
             <p className="opacity-5">Магазин лучших кроссовок</p>
